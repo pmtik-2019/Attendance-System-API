2d65df610ce597b07cdb869a79ed1147969fe418
Diana <diananur396@gmail.com>
2019-01-25 09:26:50 +0700

    CRUD absensi

--- views/admin/absensi/index.php
+++ views/admin/absensi/index.php
@@ -32,7 +32,21 @@ $this->params['breadcrumbs'][] = $this->title;
             'laporan_kerja:ntext',
             'nim',
 
-            ['class' => 'yii\grid\ActionColumn'],
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{view} {update} {delete}',
+                'buttons' => [
+                    'view' => function($url, $model) {
+                    return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', ['absensi-view', 'id' => $model['id_absensi']], ['title' => 'View', 'id' => 'modal-btn-view']);
+                    },
+                    'update' => function($id, $model) {
+                        return Html::a('<span class="glyphicon glyphicon-pencil"></span>', ['absensi-update', 'id' => $model['id_absensi']], ['title' => 'Update', 'id' => 'modal-btn-view']);
+                    },
+                    'delete' => function($url, $model) {
+                        return Html::a('<span class="glyphicon glyphicon-trash"></span>', ['absensi-delete', 'id' => $model['id_absensi']], ['title' => 'Delete', 'class' => '', 'data' => ['confirm' => 'Are you absolutely sure ? You will lose all the information about this user with this action.', 'method' => 'post', 'data-pjax' => false],]);
+                    }
+                ]
+            ],
         ],
     ]); ?>
     <?php Pjax::end(); ?>
